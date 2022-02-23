ce20b55213513c49af6fa87b0a2fefdba78b0574
Zherekhov Serhii <96929793+serhii-zherekhov@users.noreply.github.com>
2022-02-23 15:22:14 +0200

    Demo project

---
+++ Assets/MoverSystem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+public class MoverSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref Translation translation, ref MoveSpeedComponent moveSpeedComponent) =>
+        {
+            translation.Value.x += moveSpeedComponent.speedX * Time.DeltaTime;
+            translation.Value.y += moveSpeedComponent.speedY * Time.DeltaTime;
+            if (translation.Value.x > 8f)
+            {
+                moveSpeedComponent.speedX = -math.abs(moveSpeedComponent.speedX);
+            }
+            if (translation.Value.x < -8f)
+            {
+                moveSpeedComponent.speedX = +math.abs(moveSpeedComponent.speedX);
+            }
+            if (translation.Value.y > 5f)
+            {
+                moveSpeedComponent.speedY = -math.abs(moveSpeedComponent.speedY);
+            }
+            if (translation.Value.y < -5f)
+            {
+                moveSpeedComponent.speedY = +math.abs(moveSpeedComponent.speedY);
+            }
+        });
+    }
+}
---
+++ Assets/Testing.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Collections;
+using Unity.Rendering;
+using Unity.Mathematics;
+
+public class Testing : MonoBehaviour
+{
+    [SerializeField] private Mesh _mesh;
+    [SerializeField] private Material _material;
+
+
+    private void Start()
+    {
+        EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+        EntityArchetype entityArchetype = entityManager.CreateArchetype(
+            typeof(MoveSpeedComponent),
+            typeof(LevelComponent),
+            typeof(Translation),
+            typeof(RenderMesh),
+            typeof(LocalToWorld),
+            typeof(RenderBounds)
+            );
+
+        NativeArray<Entity> entityArray = new NativeArray<Entity>(1000, Allocator.Temp);
+        entityManager.CreateEntity(entityArchetype, entityArray);
+
+        foreach (Entity entity in entityArray)
+        {
+            int signX = 1;
+            int signY = 1;
+
+            if (UnityEngine.Random.Range(0, 2) == 0)
+                signX = -1;
+            if (UnityEngine.Random.Range(0, 2) == 0)
+                signY = -1;
+
+            entityManager.SetComponentData(entity, new MoveSpeedComponent { speedX = signX * UnityEngine.Random.Range(1.0f, 2.0f), speedY = signY * UnityEngine.Random.Range(1.0f, 2.0f), });
+            
+            entityManager.SetComponentData(entity, new LevelComponent { level = UnityEngine.Random.Range(0, 9999) });
+            entityManager.SetComponentData(entity, new Translation { Value = new float3(UnityEngine.Random.Range(-8f, 8f), UnityEngine.Random.Range(-5f, 5f), 0)});
+            entityManager.SetSharedComponentData(entity, new RenderMesh 
+            {
+                mesh = _mesh,
+                material = _material,
+            });
+        }
+
+        entityArray.Dispose();
+    }
+}
